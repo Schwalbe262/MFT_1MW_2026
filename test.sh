e5b61ad6678acda8d4bfdcb431d472960890ade6
Schwalbe262 <r1jae262@uos.ac.kr>
2026-03-19 03:03:38 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu4,gpu3,gpu6,gpu2,gpu1,cpu1,cpu2
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:0
+
+#SBATCH --job-name=ANSYS
+#SBATCH -o ./log/SLURM.%N.%j.out         # STDOUT
+#SBATCH -e ./log/SLURM.%N.%j.err         # STDERR
+
+module purge
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pyaedt2026v1
+
+module load ansys-electronics/v252
+
+echo "HOST=$(hostname)"
+
+# export ANSYSEM_ROOT242=/opt/ohpc/pub/Electronics/v242/Linux64
+# export PATH=$ANSYSEM_ROOT242/ansysedt/bin:$PATH
+# export ANSYSLMD_LICENSE_FILE=1055@172.16.10.81
+
+# unset DISPLAY
+# export QT_QPA_PLATFORM=offscreen
+
+# export HOME=/gpfs/home1/r1jae262
+# cd /gpfs/home1/r1jae262/ANSYS
+
+python run_simulation_normal.py
