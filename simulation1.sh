77e27bec42245f89010bba61fbe9591aa64851da
Schwalbe262 <r1jae262@uos.ac.kr>
2026-03-18 21:49:15 +0900

    Update simulation1.sh

--- simulation1.sh
+++ simulation1.sh
@@ -3,11 +3,11 @@
 #SBATCH --partition=gpu5,gpu4,gpu3,gpu6,gpu2,gpu1,cpu1,cpu2
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:0
-
 #SBATCH --job-name=ANSYS
-#SBATCH -o ./log/SLURM.%N.%j.out         # STDOUT
-#SBATCH -e ./log/SLURM.%N.%j.err         # STDERR
+#SBATCH -o ./log/SLURM.%N.%j.out
+#SBATCH -e ./log/SLURM.%N.%j.err
 
+set -euo pipefail
 
 module purge
 
@@ -15,18 +15,19 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pyaedt2026v1
 
 module load ansys-electronics/v252
-simulation1.sh
 
+export ANSYSEM_ROOT252=/opt/ohpc/pub/Electronics/v252/AnsysEM
+export ANSYSLMD_LICENSE_FILE=1055@172.16.10.81
 
-# export ANSYSEM_ROOT242=/opt/ohpc/pub/Electronics/v242/Linux64
-# export PATH=$ANSYSEM_ROOT242/ansysedt/bin:$PATH
-# export ANSYSLMD_LICENSE_FILE=1055@172.16.10.81
+mkdir -p ./log
 
-# unset DISPLAY
-# export QT_QPA_PLATFORM=offscreen
-
-
-# export HOME=/gpfs/home1/r1jae262
-# cd /gpfs/home1/r1jae262/ANSYS
+echo "HOST=$(hostname)"
+echo "CONDA_DEFAULT_ENV=$CONDA_DEFAULT_ENV"
+echo "ANSYSEM_ROOT252=$ANSYSEM_ROOT252"
+echo "ANSYSLMD_LICENSE_FILE=$ANSYSLMD_LICENSE_FILE"
+which python
+python --version
+python -c "import sys; print(sys.executable)"
+python -c "import os; print(os.path.exists('/opt/ohpc/pub/Electronics/v252/AnsysEM/ansysedt'))"
 
 python run_simulation.py
