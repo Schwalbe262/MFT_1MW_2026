e34b1fe5017ca1314b697ec83fa7e45849dbc54d
Schwalbe262 <r1jae262@uos.ac.kr>
2026-05-15 17:56:50 +0900

    Update simulation1.sh

--- simulation1.sh
+++ simulation1.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu4,gpu3,gpu6,gpu2,gpu1,cpu1
-#SBATCH --cpus-per-task=40
-#SBATCH --gres=gpu:0
-
 #SBATCH --job-name=ANSYS
-#SBATCH -o ./log/SLURM.%N.%j.out         # STDOUT
-#SBATCH -e ./log/SLURM.%N.%j.err         # STDERR
+#SBATCH -o ./log/SLURM.%N.%j.out
+#SBATCH -e ./log/SLURM.%N.%j.err
+
+mkdir -p ./log
 
 module purge
 
@@ -15,16 +17,10 @@ conda activate pyaedt2026v1
 
 module load ansys-electronics/v252
 
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
 echo "HOST=$(hostname)"
+echo "SLURM_JOB_ID=$SLURM_JOB_ID"
 
-# export ANSYSEM_ROOT242=/opt/ohpc/pub/Electronics/v242/Linux64
-# export PATH=$ANSYSEM_ROOT242/ansysedt/bin:$PATH
-# export ANSYSLMD_LICENSE_FILE=1055@172.16.10.81
-
-# unset DISPLAY
-# export QT_QPA_PLATFORM=offscreen
-
-# export HOME=/gpfs/home1/r1jae262
-# cd /gpfs/home1/r1jae262/ANSYS
-
-python subprocess_run.py
+srun --cpu-bind=cores python subprocess_run.py
