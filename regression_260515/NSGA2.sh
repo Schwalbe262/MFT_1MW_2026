a11eaeef078d858e0c043983b4cd0e8168efeda4
Schwalbe262 <r1jae262@uos.ac.kr>
2026-05-21 14:37:46 +0900

    Update NSGA2.sh

--- regression_260515/NSGA2.sh
+++ regression_260515/NSGA2.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
 #SBATCH --time=12:00:00
-#SBATCH --partition=gpu4,gpu3,gpu6,gpu2,gpu1,cpu1
+#SBATCH --partition=cpu2,gpu4,gpu3,gpu6,gpu2,gpu1,cpu1
 #SBATCH --job-name=NSGA2
 #SBATCH -o ./log/SLURM.%N.%j.out
 #SBATCH -e ./log/SLURM.%N.%j.err
